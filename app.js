e627708c963f5ee775d0315a34ca7b5dba0c5683
dnzbtc <dnzanka38@gmail.com>
2026-03-01 04:58:56 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-01T01:58:53.939Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-01T01:58:55.468Z
+Commit: Fix broken links in documentation
